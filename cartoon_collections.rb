b3cccc00f9a1757bfea516eff69e69dff3fd2dc9
Irum F <irummf@gmail.com>
2018-08-26 20:27:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ def long_planeteer_calls(calls_long)
   calls_long.any? {|word| word.length > 4} #=> true
 end 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese()
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
