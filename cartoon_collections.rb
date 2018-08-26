4da16ebe8f0fa2d58e488915cec39923a8e4ed50
Irum F <irummf@gmail.com>
2018-08-26 19:41:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map {|planeteer_call| planeteer_call.upcase +"!"} 
+  planeteer_calls.map {|planeteer_call| planeteer_call.capitalize +"!"} 
 end
 
 def long_planeteer_calls# code an argument here
